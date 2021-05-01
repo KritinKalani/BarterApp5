b1f8e112e3f2b55c2b81d8447d76a148f68ea813
KritinKalani <kritinkalani@woodstock.ac.in>
2021-05-01 11:36:00 +0530

    Barter App

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,52 +1,145 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, Modal, ScrollView, KeyboardAvoidingView } from 'react-native';
 import firebase from 'firebase'
+import db from '../config'
 import Barter from '../components/barter';
 
 
 export default class WelcomeScreen extends React.Component {
 
-constructor(){
+  constructor() {
     super();
     this.state = {
-        emailID: '',
-        password: ''
+      emailID: '',
+      password: '',
+      isModaVisible: false,
+      firstName: '',
+      lastName: '',
+      phoneNumber: '',
+      address: '',
+      confirmPassword: ''
     }
-}
+  }
 
-userLogin = (emailId, password) => {
+  userLogin = (emailId, password) => {
     firebase.auth().signInWithEmailAndPassword(emailId, password)
-        .then(() => {
-            return Alert.alert("Successfully Logged in")
-        })
-        .catch((error) => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            return Alert.alert(errorMessage)
-        })
-}
-
-userSignUp = (emailId, password)=>{
-    firebase.auth().createUserWithEmailAndPassword(emailId, password).then(()=>{
-        return Alert.alert("Successfully signed up!")
-    }
-    ).catch(function(error){
+      .then(() => {
+        return Alert.alert("Successfully Logged in")
+      })
+      .catch((error) => {
         var errorCode = error.code;
         var errorMessage = error.message;
-        return Alert.alert(errorMessage);
-    })
-}
+        return Alert.alert(errorMessage)
+      })
+  }
 
-render() {
+  userSignUp = (emailId, password, confirmPassword) => {
+    if(password!==confirmPassword){
+      return Alert.alert('Password and Confirm Password dont atch. \n Please check again')
+    }else{
+    firebase.auth().createUserWithEmailAndPassword(emailId, password).then(() => {
+      db.collection('Users').add({
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        emailID: this.state.emailID,
+        password: this.state.password,
+        phoneNumber: this.state.phoneNumber,
+        confirmPassword: this.state.confirmPassword,
+        address: this.state.address
+      })
+      return Alert.alert("Hey"+this.state.firstName+"You Have successfully signed up!")
+    }
+    ).catch(function (error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      return Alert.alert(errorMessage);
+    })}
+  }
+
+  showModal = () => {
+    return (
+      <Modal
+        animationType="fade"
+        visible={this.state.isModaVisible}
+        transparent={true}>
+        <View style = {{width: 300}}>
+          <ScrollView>
+            <KeyboardAvoidingView>
+              <Text>Sign Up</Text>
+              <TextInput
+            style={{ width: '100%', height: 30, marginTop: 30, marginBottom: 30 }}
+                placeholder="First Name"
+                maxLength={10}
+                onChangeText={(text) => {
+                  this.setState({ firstName: text })
+                }} />
+              <TextInput
+              style={{ width: '100%', height: 30, marginTop: 30, marginBottom: 30 }}
+                placeholder="Last Name"
+                maxLength={10}
+                onChangeText={(text) => {
+                  this.setState({ lastName: text })
+                }} />
+              <TextInput
+              style={{ width: '100%', height: 30, marginTop: 30, marginBottom: 30 }}
+                placeholder="Phone Number"
+                keyboardType={'numeric'}
+                maxLength={10}
+                onChangeText={(text) => {
+                  this.setState({ phoneNumber: text })
+                }} />
+              <TextInput
+              style={{ width: '100%', height: 30, marginTop: 30, marginBottom: 30 }}
+                placeholder="emailID"
+                keyboardType={'email-address'}
+                onChangeText={(text) => {
+                  this.setState({ emailID: text })
+                }} />
+              <TextInput
+              style={{ width: '100%', height: 30, marginTop: 30, marginBottom: 30 }}
+                placeholder="Address"
+                multiline={true}
+                onChangeText={(text) => {
+                  this.setState({ address: text })
+                }} />
+              <TextInput
+              style={{ width: '100%', height: 30, marginTop: 30, marginBottom: 30 }}
+                placeholder="Password"
+                secureTextEntry={true}
+                maxLength={10}
+                onChangeText={(text) => {
+                  this.setState({ password: text })
+                }} />
+              <TextInput
+              style={{ width: '100%', height: 30, marginTop: 30, marginBottom: 30 }}
+                placeholder="Confirm Password"
+                secureTextEntry={true}
+                maxLength={10}
+                onChangeText={(text) => {
+                  this.setState({ confirmPassword: text })
+                }} />
+                <View>
+              <TouchableOpacity style={{ width: 100, height: 20, alignItems: 'center', alignSelf: 'center', marginTop: 30 }} onPress={this.userSignUp(this.state.emailID, this.state.password, this.state.confirmPassword)}><Text>Sign Up</Text></TouchableOpacity></View>
+              <View><TouchableOpacity style={{ width: 100, height: 20, alignItems: 'center', alignSelf: 'center', marginTop: 30 }} onPress = {()=>{
+                this.setState({isModaVisible: false})
+              }}><Text>Cancel</Text></TouchableOpacity></View>
+            </KeyboardAvoidingView>
+          </ScrollView>
+        </View>
+      </Modal>
+    )
+  }
+
+  render() {
     return (
       <View>
         <View>
-            <Barter/>
-          <Text style = {{fontFamily: 'cursive', fontSize: 25, textAlign: 'center', marginTop: 20, marginBottom: 10}}>Welcome to the Barter App!!!</Text>
+          <Text style={{ fontFamily: 'cursive', fontSize: 25, textAlign: 'center', marginTop: 20, marginBottom: 10 }}>Welcome to the Barter App!!!</Text>
         </View>
+          {this.showModal()}
         <View>
           <TextInput
-          style = {{width: '70%', height: 35, marginTop: 30}}
+            style={{ width: '50%', height: 35, marginTop: -750, alignSelf: 'center', alignItems: 'center' }}
             placeholder="abc@xyz.com"
             keyboardType="email-address"
             onChangeText={(text) => {
@@ -54,21 +147,25 @@ render() {
             }}
           />
           <TextInput
-         style = {{width: '70%', height: 35, marginTop: 30, marginBottom: 50}}
+            style={{ width: '50%', height: 35, marginTop: 30, alignSelf: 'center', alignItems: 'center'}}
+            placeholder = "Password"
+            secureTextEntry = {true}
             secureTextEntry={true}
             onChangeText={(text) => {
               this.setState({ password: text });
             }}
           />
           <TouchableOpacity
-          style = {{width: 100, height: 20, alignItems: 'center', alignSelf: 'center', marginTop: 30}}
+            style={{ width: 100, height: 20, alignItems: 'center', alignSelf: 'center', marginTop: 30 }}
             onPress={this.userLogin(this.state.emailID, this.state.password)}>
-            <Text style = {{fontSize: 20, fontFamily: 'bold'}}>Login</Text>
+            <Text style={{ fontSize: 20, fontFamily: 'bold' }}>Login</Text>
           </TouchableOpacity>
           <TouchableOpacity
-          style = {{width: 100, height: 20, alignItems: 'center', alignSelf: 'center', marginTop: 30}}
-            onPress={this.userLogin(this.state.emailID, this.state.password)}>
-            <Text style = {{fontSize: 20, fontFamily: 'bold'}}>SignUp</Text>
+            style={{ width: 100, height: 20, alignItems: 'center', alignSelf: 'center', marginTop: 30 }}
+            onPress={()=>{
+              this.setState({isModaVisible: true})
+            }}>
+            <Text style={{ fontSize: 20, fontFamily: 'bold' }}>SignUp</Text>
           </TouchableOpacity>
         </View>
       </View>
